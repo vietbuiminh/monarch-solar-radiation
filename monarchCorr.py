0fab0352acb2b833ab9c4e46475257c4d72f087e
Tyler Fulghum <51171819+tfulghum@users.noreply.github.com>
2023-05-23 10:25:48 -0500

    Moved log folder, added new logs, added a readme for the info on logs

--- monarchCorr.py
+++ monarchCorr.py
@@ -8,22 +8,23 @@ from sklearn.model_selection import train_test_split
 import statsmodels.api as sm
 
 #  Reading in log file
-df = pd.read_csv('Log00004.csv') 
-#df2 = pd.read_csv('Log00002.csv')
-#df3 = pd.read_csv('Log00003.csv')
-#df4 = pd.read_csv('Log00004.csv')
-#frames = [df1, df2, df4]
-#df = pd.concat(frames)
+df1 = pd.read_csv('Logs/Log00007.csv') 
+df2 = pd.read_csv('Logs/Log00008.csv')
+df3 = pd.read_csv('Logs/Log00002.csv')
+df4 = pd.read_csv('Logs/Log00004.csv')
+frames = [df1, df2, df3, df4]
+df = pd.concat(frames)
+
+t_fract = df['Time [UTC]']
+t_fract = (t_fract.str[11:13])
+t_fract = t_fract.astype(int)
+# + t_fract.str[14:16]
+
 
 # Removing battery level, roll, pitch, K&Z Temp, and NA row that was added as a buffer.
 df = df.iloc[: ,2:]
 df = df.drop(columns=[" Bat [V]", " R_u [deg]", " P_u [deg]"])
 df = df.iloc[: ,:-2]
-#print(df)
-
-# Seaborn Hearmap
-#sns.heatmap(df.corr());
-#plt.show()
 
 #dfCorr = (df.corr())
 #dfCorr.to_excel('test.xlsx', sheet_name='sheet1', index=True)
@@ -44,7 +45,8 @@ df['y_hats'] = y_hats2
 
 # Plot colors of points based on time stamp
 # Assuming everything in order
-t_fract = df.index / df.index.max()
+# Commented out, easy time fract by order
+#t_fract = df.index / df.index.max()
 #color = 1 - (t_fract * 0.8 + 0.1)
 
 
@@ -54,6 +56,7 @@ for col in cols:
     i += 1
     ax = plt.subplot(3, 2, i)
     scatter = ax.scatter( df[' Pyro [uV]'], df[col], c=t_fract, alpha=0.7 )
+    # Show estimates here
     #ax.plot( df['y_hats'], df[col], 'ko', color = 'red', alpha=0.05)
     ax.set_xlabel(' Pyro [uV]', fontweight='bold')
     ax.set_ylabel(col, fontweight='bold')
