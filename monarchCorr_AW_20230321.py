5ed85ba8ee73308fe8994252d768aefa520655ba
Tyler Fulghum <51171819+tfulghum@users.noreply.github.com>
2023-06-19 21:36:16 -0500

    added notebook

--- monarchCorr_AW_20230321.py
+++ monarchCorr_AW_20230321.py
@@ -8,12 +8,7 @@ from sklearn.model_selection import train_test_split
 import statsmodels.api as sm
 
 #  Reading in log file
-#df1 = pd.read_csv('Log00001.csv') 
-df2 = pd.read_csv('Log00002.csv')
-#df3 = pd.read_csv('Log00003.csv')
-df4 = pd.read_csv('Log00004.csv')
-frames = [df4]
-df = pd.concat(frames)
+df = pd.read_csv('Logs/Log00020.csv') 
 
 # Removing battery level, roll, pitch, K&Z Temp, and NA row that was added as a buffer.
 t_fract = df['Time [UTC]']
@@ -21,7 +16,7 @@ t_fract = (t_fract.str[11:13])
 t_fract = t_fract.astype(int)
 
 # Zenith Angle
-h = (t - 12) / 12
+#h = (t - 12) / 12
 
 df = df.iloc[: ,2:]
 df = df.drop(columns=[" Bat [V]", " R_u [deg]", " P_u [deg]"])
@@ -41,17 +36,6 @@ cols = [' UVA_u', ' UVB_u', ' White_u', ' Vis_u [lx]', ' IR_S_u',
        
 Y = df[[' Pyro [uV]']]
 X = df.drop(columns=[' Pyro [uV]'])
-#(X_train, X_test, y_train, y_test) = train_test_split(X, Y, train_size = 0.8)
-
-regr = linear_model.LinearRegression()
-regr.fit(X, Y) 
-
-y_hats2 = regr.predict(X)
-df['y_hats'] = y_hats2
-#12 13
-# Plot colors of points based on time stamp
-# Assuming everything in order
-#t_fract = df.index / df.index.max()
 
 #color = 1 - (t_fract * 0.8 + 0.1)
 color = t_fract
