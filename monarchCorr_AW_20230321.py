866b91b57329bf9cafba73cafd00dc69f4ab8e26
Viet Bui <buiminhviet.2002@gmail.com>
2024-09-04 15:14:44 -0500

    Code updated with regex format to save the column title without the extra space in front, and query to have data without the error of 65535

--- monarchCorr_AW_20230321.py
+++ monarchCorr_AW_20230321.py
@@ -2,13 +2,22 @@
 
 import pandas as pd
 from matplotlib import pyplot as plt
-import seaborn as sns
+# import seaborn as sns
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
-import statsmodels.api as sm
+# import statsmodels.api as sm
+import duckdb
 
 #  Reading in log file
-df = pd.read_csv('Logs/Log00020.csv') 
+df = pd.read_csv('Logs/Log00020.csv')
+
+# remove the extra spacing infront of the heading of the columns
+df.columns = df.columns.str.replace(r'^\s+', '', regex=True)
+df['White_u'] = df['White_u'].astype('int')
+print(df['White_u'])
+
+df = duckdb.query("SELECT  *  FROM df WHERE White_u < 65535").to_df()
+print(df)
 
 # Removing battery level, roll, pitch, K&Z Temp, and NA row that was added as a buffer.
 t_fract = df['Time [UTC]']
@@ -16,41 +25,40 @@ t_fract = (t_fract.str[11:13])
 t_fract = t_fract.astype(int)
 
 # Zenith Angle
-#h = (t - 12) / 12
+# h = (t - 12) / 12
 
-df = df.iloc[: ,2:]
-df = df.drop(columns=[" Bat [V]", " R_u [deg]", " P_u [deg]"])
-df = df.iloc[: ,:-2]
-#print(df)
+df = df.iloc[:, 2:]
+df = df.drop(columns=["Bat [V]", "R_u [deg]", "P_u [deg]"])
+df = df.iloc[:, :-2]
+# print(df)
 
 # Seaborn Hearmap
-#sns.heatmap(df.corr());
-#plt.show()
+# sns.heatmap(df.corr());
+# plt.show()
 
-#dfCorr = (df.corr())
-#dfCorr.to_excel('test.xlsx', sheet_name='sheet1', index=True)
+# dfCorr = (df.corr())
+# dfCorr.to_excel('test.xlsx', sheet_name='sheet1', index=True)
 
 # 6 columns
-cols = [' UVA_u', ' UVB_u', ' White_u', ' Vis_u [lx]', ' IR_S_u',
-       ' IR_M_u']
-       
-Y = df[[' Pyro [uV]']]
-X = df.drop(columns=[' Pyro [uV]'])
+cols = ['UVA_u', 'UVB_u', 'White_u', 'Vis_u [lx]', 'IR_S_u',
+        'IR_M_u']
 
-#color = 1 - (t_fract * 0.8 + 0.1)
+Y = df[['Pyro [uV]']]
+X = df.drop(columns=['Pyro [uV]'])
+
+# color = 1 - (t_fract * 0.8 + 0.1)
 color = t_fract
 
-fig = plt.figure(figsize=(8,10))
-i=0
+fig = plt.figure(figsize=(8, 10))
+i = 0
 for col in cols:
     i += 1
     ax = plt.subplot(3, 2, i)
-    scatter = ax.scatter( df[' Pyro [uV]'], df[col], c=t_fract, alpha=0.7 )
-    #ax.plot( df['y_hats'], df[col], 'ko', color = 'red', alpha=0.05)
-    ax.set_xlabel(' Pyro [uV]', fontweight='bold')
+    scatter = ax.scatter(df['Pyro [uV]'], df[col], c=t_fract, alpha=0.05, s=2)
+    # ax.plot( df['y_hats'], df[col], 'ko', color = 'red', alpha=0.05)
+    ax.set_xlabel('Pyro [uV]', fontweight='bold')
     ax.set_ylabel(col, fontweight='bold')
     fig.colorbar(scatter, location='bottom')
-    
+
 plt.tight_layout()
 plt.show()
-
