b0ab8ced9f13e792acb3b7a5553f6db17550ef32
Viet Bui <buiminhviet.2002@gmail.com>
2024-10-01 13:04:14 -0500

    Added the Arduino script for the data logger into the project repo

---
+++ monarch_margay_logger/monarch_margay_logger.ino
@@ -0,0 +1,38 @@
+#include "Margay.h"
+#include <DysonSW.h>
+#include "MCP3421.h"  //Inlcude ADC lib
+// make sure to have this folder inside the Arduino Library inside Document Folder 
+// https://github.com/NorthernWidget-Skunkworks/Monarch_Library/tree/Dev_Polling
+
+MCP3421 CMP3(0x6B);  //Init with addres 0x6B
+
+DysonSW PyroUp(UP); //Initialzie Upward facing Monarch short wave
+
+String Header = "Pyro [uV], "; //Information header
+uint8_t I2CVals[3] = {0x40, 0x1D, 0x6B}; 
+unsigned long UpdateRate = 15; //Number of seconds between readings 
+
+Margay Logger(MODEL_2v2);
+
+void setup() {
+	Header = Header + PyroUp.GetHeader();
+	Logger.begin(I2CVals, sizeof(I2CVals), Header); //Pass header info to logger
+	Init();
+}
+
+void loop() {
+	Logger.run(Update, UpdateRate);
+}
+
+String Update() {
+	Init(); //DEBUG!
+	float Val1 = CMP3.GetVoltage()*(1.0e4); //Return val in uV, account for gain of amp (100 V/V)
+	return String(Val1) + "," + PyroUp.GetString();
+}
+
+void Init() 
+{
+	PyroUp.begin();
+	CMP3.Begin();
+	CMP3.SetResolution(18); //Set for full 18 bit resolution
+}
