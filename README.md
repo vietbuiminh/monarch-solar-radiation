47090cc95bca81e42ee424a3d95b3a2a75d7f9d2
Viet Bui <buiminhviet.2002@gmail.com>
2024-10-01 12:50:32 -0500

    New update with new graphs, the station is running again to collect more data in 2024

--- README.md
+++ README.md
@@ -3,26 +3,37 @@
 
 ---
 **Last updated:** 
-2024-09-17 
-![Zeros in IR S in bins](zeros_in_IR_S_u.png)
+2024-10-01
+The station has been setup and currently running on top of SAFL. It will be running for a couple of days. The goal is to check if the same error reproduced in this new run.
+
+![](graphs/filtered_error_irs_temp.png)
+![](graphs/error_irs_temp.png)
+
+This graph shows a better pattern for the IR Short thru Time and Temperature thru Time.
+
+It seems to me that if the Temperature drop bellow 20 C probably at night and early morning then the sensors pick up as 0 but that pattern works only for the first couple of days.
+
+Here is the graph after I only include the data between hour 9 to 17 which does reduced the number of 0 reading from IR Shortwave.
+
+![Zeros in IR S in bins](graphs/zeros_in_IR_S_u.png)
 
 I was wondering if the the zeros from IR short wave frequently around a specific time of the day but turn out after I bin them into hours and bar graph all of the zeros with high Pyrometer values, the patterns tells me it happened a lot around the early hours and interestingly the whole day of 25 of June. I checked the photos of the sky it looks blue and with normal clouds. Still don't know why, perhaps this is due to hardware issue?
 
-![Sky BGR signature](completeskysignature.png)
+![Sky BGR signature](graphs/completeskysignature.png)
 
 *Figure 1: Sky BRG Signature between the 7 June to 28 August*
 
 Interesting finding: between the 14 and 16 of June, I checked with the past weather and they reported those days to have a lot of smoke in the air.
 We can also see the day light getting shorter approaching August.
 
-![Sensors data](convertUTC_hour6_to_hour17.png)
+![Sensors data](graphs/convertUTC_hour6_to_hour17.png)
 
 *Figure 2: Sensors data v Pyro between the hour 6 to hour 17*
 
 Converted UTC to Central Time and only select data between 6 AM and 5 PM
 Data is filtered to remove error data which usually occur by hardware or during night time. Still need to check on why short wavelength of IR caused the staircase patterns.
 
-![Blue Value vs Pyros](blue_v_pyro_hour6_to_hour17.png)
+![Blue Value vs Pyros](graphs/blue_v_pyro_hour6_to_hour17.png)
 
 *Figure 3: Blue value vs Pyro between the 7 to 30*
 
--- monarchCorr_AW_20230321.py
+++ monarchCorr_AW_20230321.py
@@ -99,20 +99,23 @@ df['White_u'] = df['White_u'].astype('float').round().astype('int')
 df['Pyro [uV]'] = df['Pyro [uV]'].astype('float').round().astype('int')
 df['IR_M_u'] = df['IR_M_u'].astype('float')
 
-print(df['White_u'])
+# print(df['White_u'])
 
 df['Time [UTC]'] = pd.to_datetime(df["Time [UTC]"])
 df['Time [UTC]'] = df['Time [UTC]'].dt.tz_localize('UTC')
 df['time'] = df['Time [UTC]'].dt.tz_convert(
     'America/Chicago')
-df = df[(df['time'].dt.hour >= 6) & (df['time'].dt.hour <= 17)]
+
+# filter, you can comment this line to view the full data
+# df = df[(df['time'].dt.hour >= 9) & (df['time'].dt.hour <= 17)]
 df = duckdb.query("SELECT  *  FROM df WHERE White_u < 65535").to_df()
+# print(df['Time [UTC]'])
 weird_date = duckdb.query(
     'SELECT "time", "Pyro [uV]", "IR_M_u" FROM df WHERE ((IR_M_u < 1.5 AND IR_M_u > 0.5) AND ("Pyro [uV]" < 8000 AND "Pyro [uV]" > 2000))').to_df()
-print(weird_date)
+# print(weird_date)
 another_weird_date = duckdb.query(
     'SELECT "time", "Pyro [uV]", "IR_M_u" FROM df WHERE ((IR_M_u < 1.56 AND IR_M_u > 0.13) AND ("Pyro [uV]" < 250 AND "Pyro [uV]" > -67))').to_df()
-print(another_weird_date)
+# print(another_weird_date)
 inves_zero = duckdb.query(
     'SELECT "time", "Pyro [uV]", "IR_S_u" FROM df WHERE (IR_S_u <= 0)').to_df()
 print(inves_zero)
@@ -131,16 +134,45 @@ pivot_data = grouped_data.pivot(
     index='date', columns='hour', values='count').fillna(0)
 
 # Plot the bar graph for each date
-pivot_data.plot(kind='bar', stacked=True, figsize=(12, 8), colormap='viridis')
-plt.xlabel('Date', fontweight='bold')
-plt.ylabel('Total Count', fontweight='bold')
-plt.title('Total Count by Hour and Date', fontweight='bold')
-plt.legend(title='Hour of the Day', bbox_to_anchor=(1.05, 1), loc='upper left')
-plt.title('Total Count by Hour of the Day', fontweight='bold')
-plt.xticks(range(0, 24))  # Ensure all hours are shown on the x-axis
-plt.grid(axis='y', linestyle='--', alpha=0.7)
-plt.tight_layout()
+# pivot_data.plot(kind='bar', stacked=True, figsize=(12, 8), colormap='viridis')
+# plt.xlabel('Date', fontweight='bold')
+# plt.ylabel('Total Count', fontweight='bold')
+# plt.title('Total Count by Hour and Date', fontweight='bold')
+# plt.legend(title='Hour of the Day', bbox_to_anchor=(1.05, 1), loc='upper left')
+# plt.title('Total Count by Hour of the Day', fontweight='bold')
+# plt.xticks(range(0, 24))  # Ensure all hours are shown on the x-axis
+# plt.grid(axis='y', linestyle='--', alpha=0.7)
+# plt.tight_layout()
+# plt.show()
+print("This is the first line")
+print(df['time'])
+
+
+df['IR_S_u_sim'] = (df['IR_S_u'] > 0).astype(int)
+print(df['IR_S_u_sim'])
+df['PyroT_u [C]'] = df['PyroT_u [C]'].astype(float)
+temperature = df['PyroT_u [C]']
+print(temperature)
+IR_sim = df['IR_S_u_sim']
+
+fig, ax1 = plt.subplots()
+
+ax1.plot(df['time'], IR_sim, c='r')
+ax1.set_xlabel('Time', fontweight='bold')
+ax1.set_ylabel('IR Short Wave Reading (0/1)', fontweight='bold')
+ax1.tick_params(axis='y', labelcolor='r')
+# ax1.set_title('Temperature vs. Simplified IR Readings thru Time',
+#               fontweight='bold')
+# ax1.grid(True)
+
+ax2 = ax1.twinx()
+ax2.set_ylabel('Temperature')
+ax2.scatter(df['time'], temperature, c='b', s=1)
+ax2.tick_params(axis='y', labelcolor='tab:blue')
+
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
 plt.show()
+
 
 # Removing battery level, roll, pitch, K&Z Temp, and NA row that was added as a buffer.
 t_fract = df['time'].astype(str)
@@ -153,6 +185,7 @@ t_fract = t_fract.astype(int)
 df = df.iloc[:, 2:]
 df = df.drop(columns=["Bat [V]", "R_u [deg]", "P_u [deg]"])
 df = df.iloc[:, :-2]
+
 # print(df)
 
 # Seaborn Hearmap
@@ -165,7 +198,7 @@ df = df.iloc[:, :-2]
 # 6 columns
 cols = ['UVA_u', 'UVB_u', 'White_u', 'Vis_u [lx]', 'IR_S_u',
         'IR_M_u']
-
+Y_extra = df[['PyroT_u [C]']]
 Y = df[['Pyro [uV]']]
 X = df.drop(columns=['Pyro [uV]'])
 
